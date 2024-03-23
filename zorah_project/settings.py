cf2e6bbef1701284e59975d549b2ad5664f6c518
Kurller <kolaquadry@gmail.com>
2024-03-23 19:09:17 +0100

    my commit

--- zorah_project/settings.py
+++ zorah_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lo7ag)oqk9@t@_des0=kmtv%o1&=z@vxa1e5_9f650+i**bu=y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
